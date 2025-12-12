6ed44304aa647b2b882eaad25f48ee74810fdc2a
sandjonyves <117326116+sandjonyves@users.noreply.github.com>
2025-12-12 23:46:12 +0100

    change database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,27 +1,19 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
-  
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  'postgresql://neondb_owner:npg_qe2zxvfolyL0@ep-wandering-firefly-ahubgluo-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: 'postgres',
-    logging: false,
-    define: {
-      freezeTableName: true,
-      underscored: false,
-      timestamps: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     },
+    logging: false,
   }
 );
 
-// Test de connexion
-sequelize.authenticate()
-  .then(() => console.log('✅ Connexion DB OK !'))
-  .catch(err => console.error('❌ Erreur DB:', err));
+sequelize.createSchema('auth', { logging: false }).catch(() => {});
 
 module.exports = sequelize;
